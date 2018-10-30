4f2994934a8f4b7b3c3112e8905277dcdc0d43cc
symynjuk <mike.thats.my.name@gmail.com>
2018-10-30 14:57:43 +0300

    symyniuk thymeleaf task

--- src/main/java/com/thymeleaf/course/domain/service/UserService.java
+++ src/main/java/com/thymeleaf/course/domain/service/UserService.java
@@ -1,13 +1,16 @@
 package com.thymeleaf.course.domain.service;
 
 import com.thymeleaf.course.domain.model.dto.UserSignUpRequest;
+import com.thymeleaf.course.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
-
+    @Autowired
+    UserRepository userRepository;
     public void saveUser(UserSignUpRequest request) {
        userRepository.save(request);
     }
